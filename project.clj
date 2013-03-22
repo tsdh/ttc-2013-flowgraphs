a4a885af9132a55e4d3f89cd5dd26a0260fb14b3
Tassilo Horn <horn@uni-koblenz.de>
2013-03-22 14:43:19 +0100

    add HP

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject ttc-2013-flowgraphs "0.0.1"
   :description "A Solution for the TTC 2013 Flowgraphs Case."
+  :url "https://github.com/tsdh/ttc-2013-flowgraphs"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [funnyqt "0.4.23"]]
   :license {:name "GNU General Public License, Version 3"
