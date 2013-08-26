f05d2a43b659a64422b2c929be698dffcad73e84
Tassilo Horn <horn@uni-koblenz.de>
2013-08-26 16:24:44 +0200

    more fixes

--- postproc/lst/04.clj
+++ postproc/lst/04.clj
@@ -1,8 +1,8 @@
   (^:top method2method [m]
          :from 'members.ClassMethod
-         :to [fgm 'flowgraph.Method, ex 'flowgraph.Exit]
+         :to [fgm 'flowgraph.Method, fgex 'flowgraph.Exit]
          (eset! fgm :txt (stmt2str m)) ;; Invoke the model-to-text transformation
-         (eset! ex :txt "Exit")
-         (eset! fgm :exit ex)
+         (eset! fgex :txt "Exit")
+         (eset! fgm :exit fgex)
          (eset! fgm :stmts (map stmt2item (eget m :statements)))  ;; transform the statements
          (eset! fgm :def (map param2param (eget m :parameters)))) ;; transform the parameters
